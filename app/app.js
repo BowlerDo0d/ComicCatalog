27aa62a519bfa5a21ae62873b6413bc80edbf404
Steve Mahony <smahony22@gmail.com>
2015-11-26 14:50:58 -0500

    First iteration of basic boilerplate setup

---
+++ app/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+require('angular');
+var MainController = require('./controllers/MainController');
+
+var app = angular.module('BANG-Boilerpate', []);
+
+app.controller('MainController', MainController);
---
+++ app/controllers/MainController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* @ngInject */
+function MainController() {
+  var vm = this;
+  vm.message = 'Angular Works!!!';
+}
+
+module.exports = MainController;
