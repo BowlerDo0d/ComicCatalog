1a34fa11a473347e27eadc00af09ccf6831f9a0e
Steve Mahony <smahony22@gmail.com>
2015-12-04 17:53:09 -0500

    Added Angular UI Router

--- app/app.js
+++ app/app.js
@@ -7,8 +7,33 @@ window.jQuery = $ = require('jQuery');
 require('bootstrap-sass');
 
 var angular = require('angular');
+var uiRouter = require('angular-ui-router');
 var MainController = require('./controllers/MainController');
 
-var app = angular.module('BANG-Boilerpate', []);
+var app = angular.module('BANG-Boilerpate', [ uiRouter ]);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+  // Un-matched URL
+  $urlRouterProvider.otherwise('/state1');
+
+  // Setup the states
+  $stateProvider
+    .state('state1', {
+      url: '/state1',
+      templateUrl: 'partials/state1.html'
+    })
+    .state('state1.list', {
+      url: '/list',
+      templateUrl: 'partials/state1.list.html'
+    })
+    .state('state2', {
+      url: '/state2',
+      templateUrl: 'partials/state2.html'
+    })
+    .state('state2.list', {
+      url: '/list',
+      templateUrl: 'partials/state2.list.html'
+    });
+});
 
 app.controller('MainController', MainController);
--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/* @ngInject */
 function MainController() {
   var vm = this;
   vm.message = 'Angular Works!!!';
